cbc8f9ef04cc75fc077bbf294acad12c466ee826
mashlin <mashlinr@gmail.com>
2026-06-05 15:22:54 +0100

    Add files via upload

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        private static void Main1(string[] args)
+        {
+            Console.WriteLine("Enter the number you want to learn the times tables of:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine(i + " * " + num + " = " + (num * i));
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Class4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BasketProgram
+{
+    internal class Program
+    {
+        static void Main5(string[] args)
+        {
+            int itemCount = 0;
+            double totalPrice = 0;
+            string response;
+
+            do
+            {
+                Console.WriteLine("Do you want to put an item in your basket - respond Y for yes and N for No");
+                response = Console.ReadLine().ToUpper();
+
+                if (response == "Y")
+                {
+                    Console.WriteLine("What is the price of your item?");
+                    double price = Convert.ToDouble(Console.ReadLine());
+
+                    totalPrice += price;
+                    itemCount++;
+                }
+
+            } while (response == "Y");
+
+            Console.WriteLine($"You have {itemCount} items and the value of your basket is {totalPrice:F2}");
+        }
+    }
+}
